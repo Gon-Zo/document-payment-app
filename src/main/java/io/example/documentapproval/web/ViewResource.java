007abd7ba22170972f0ade304b5c4e86920904e0
gon-zo <pnw123456@icloud.com>
2022-01-22 11:24:59 +0900

    refactor: home 에 out-box 데이터 출력 (#3)

--- src/main/java/io/example/documentapproval/web/ViewResource.java
+++ src/main/java/io/example/documentapproval/web/ViewResource.java
@@ -1,16 +1,33 @@
 package io.example.documentapproval.web;
 
+import io.example.documentapproval.service.DocumentBoxService;
 import io.example.documentapproval.service.dto.UserDTO;
+import io.example.documentapproval.service.projection.IDocumentBox;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ViewResource {
 
-  @GetMapping("/login")
-  public String showLoginView(Model model) {
-    model.addAttribute("dto", new UserDTO());
-    return "login";
-  }
+    private final DocumentBoxService service;
+
+    @GetMapping("/login")
+    public String showLoginView(Model model) {
+        model.addAttribute("dto", new UserDTO());
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String showOutBoxDocuments(Model model) {
+        List<IDocumentBox> data = service.getOutBoxDocumentList();
+        model.addAttribute("list", data);
+        model.addAttribute("type", "OUTBOX");
+        return "home";
+    }
+
 }
