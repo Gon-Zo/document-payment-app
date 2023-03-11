86affb4717ff5ed7f3e31ae31c10bcd1c4e34d08
spark <pnw123456@icloud.com>
2023-03-11 15:06:43 +0900

    feat: POST EndPoint Add
    - post request body example code add

--- src/main/kotlin/com/example/hello/HelloRouter.kt
+++ src/main/kotlin/com/example/hello/HelloRouter.kt
@@ -1,7 +1,10 @@
 package com.example.hello
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
@@ -9,6 +12,10 @@ import io.ktor.server.routing.patch
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import io.ktor.server.routing.route
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Customer(val name: String, val content: String)
 
 fun Route.customerRouting() {
     route("/customer") {
@@ -23,7 +30,8 @@ fun Route.customerRouting() {
         get("{id?}") {
         }
         post {
-
+            val customer = call.receive<Customer>()
+            call.respond(status = HttpStatusCode.Created, message = customer)
         }
         patch {
 
