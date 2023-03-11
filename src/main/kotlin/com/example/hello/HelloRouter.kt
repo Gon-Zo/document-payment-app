021f2aa1e8b9ce64f5e44ecddb6a49ff6e5f3ba7
spark <pnw123456@icloud.com>
2023-03-11 13:12:58 +0900

    feat: controller add
    - 컨트롤러 추가
    - response body and request 추가 작어

---
+++ src/main/kotlin/com/example/hello/HelloRouter.kt
@@ -0,0 +1,31 @@
+package com.example.hello
+
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+
+            val map = mapOf(
+                "test" to "test"
+            )
+
+            call.respond(map)
+        }
+        get("{id?}") {
+
+        }
+        post {
+
+        }
+        delete("{id?}") {
+
+        }
+    }
+}
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,20 @@
 package com.example.plugins
 
+import com.example.hello.customerRouting
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        customerRouting()
+    }
+}
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
     }
 }
