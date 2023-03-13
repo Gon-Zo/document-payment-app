993759d0f64ece4e178ca033106f57109ef4d13e
spark <pnw123456@icloud.com>
2023-03-13 21:37:40 +0900

    build: io.ktor.server.netty.EngineMain add
    - 톰켓으로 되어 있던 엔진 Main 변경 Netty 로

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,16 @@ plugins {
 
 group = "com.example"
 version = "0.0.1"
-application {
-    mainClass.set("io.ktor.server.tomcat.EngineMain")
 
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 dependencies {
