3c55034004d9be2d650dd4f9eca331e3ffad36d6
spark <pnw123456@icloud.com>
2023-10-08 19:31:10 +0900

    build: init project

--- src/main/kotlin/com/example/order/OrderApplication.kt
+++ src/main/kotlin/com/example/order/OrderApplication.kt
@@ -1,4 +1,4 @@
-package com.example.document
+package com.example.order
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
